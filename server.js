41e943e63583dc93aa5a50ead743795206083095
sheikb08 <sheikb08@aol.com>
2020-11-27 12:25:07 -0800

    Fitness tracker completion

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+// Create app using the express packasge
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//Establishes mongoose db connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+// Listen to request 
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
