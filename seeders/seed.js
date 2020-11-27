68ec383de746d01a805010d13482f7d1392760c9
sheikb08 <sheikb08@aol.com>
2020-11-27 12:59:31 -0800

    deploy

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let db = require("../models/exercise");
 
 // Mongoose connection to database
-mongoose.connect("mongodb://localhost/fitnesstracker", {
+mongoose.connect("mongodb://localhost/FitnessTracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
